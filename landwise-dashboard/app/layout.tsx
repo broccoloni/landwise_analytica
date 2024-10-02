6e463fa8337d3d4a4b1dc5a33e2958876a2ed67f
broccoloni <liamgraham123@gmail.com>
2024-10-01 22:52:35 -0700

    added favicon

--- landwise-dashboard/app/layout.tsx
+++ landwise-dashboard/app/layout.tsx
@@ -3,6 +3,9 @@ import '@/ui/globals.css';
 export const metadata = {
   title: 'Landwise Analytica',
   description: 'Landwise Analytica Website',
+  icons: {
+    icon: '/favicon.svg'
+  },
 };
 
 export default function RootLayout({
