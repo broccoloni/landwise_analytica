769e54b4feedbdcb7e31fc5571487e1eb8d4527d
broccoloni <liamgraham123@gmail.com>
2024-12-21 02:12:42 -0800

    changed some of the subscription items

--- beta/components/.ipynb_checkpoints/Dropdown-checkpoint.tsx
+++ beta/components/.ipynb_checkpoints/Dropdown-checkpoint.tsx
@@ -48,7 +48,7 @@ const Dropdown: React.FC<DropdownProps> = ({
         className="flex items-center pl-4 pr-2 py-2 bg-gray-200 text-black rounded focus:outline-none"
       >
         <div className="flex justify-between items-start space-x-4">
-          <span className="whitespace-pre">{selected}</span>
+          <span className="">{selected}</span>
           {(() => {
             const index = options.findIndex((option) => option === selected);
             return index >= 0 && optionsDetails[index] ? (
@@ -78,7 +78,7 @@ const Dropdown: React.FC<DropdownProps> = ({
               }`}
             >
               <div className="flex justify-between items-start w-full space-x-4">
-                <div className="whitespace-pre">{option}</div>
+                <div className="">{option}</div>
                 {optionsDetails.length > 0 && (
                   <>
                     {optionsDetails[index]}
